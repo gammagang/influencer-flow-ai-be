8645b16588d10640f0453c90915f4baaefdb00c8
gammaSpeck <madhukm3141@gmail.com>
2025-05-31 23:38:41 +0530

    feat: add content routes and update Swagger documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { campaignsRouter } from './campaigns/route'
 import { creatorsRouter } from './creators/route'
 import { campaignCreatorRouter } from './campaign-creator/route'
 import { contractsRouter } from './contracts/route'
+import { contentRouter } from './content/route'
 
 const router = Router()
 
@@ -12,5 +13,6 @@ router.use('/campaigns', campaignsRouter)
 router.use('/creators', creatorsRouter)
 router.use('/campaign-creator', campaignCreatorRouter)
 router.use('/contracts', contractsRouter)
+router.use('/content', contentRouter)
 
 export { router as allRoutes }
