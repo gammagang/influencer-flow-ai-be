40b310cd3a07bb45e572da0416bc87fb2a6e3346
gammaSpeck <madhukm3141@gmail.com>
2025-06-02 01:20:19 +0530

    fix(validate-request): remove schema from log output in validateRequest function

--- src/middlewares/validate-request.ts
+++ src/middlewares/validate-request.ts
@@ -3,7 +3,7 @@ import { log } from '@/libs/logger'
 import { RequestValidationError } from '@/errors/request-validation-error'
 
 export function validateRequest<T>(schema: Schema<T>, payload: any, instance: string) {
-  log.info('Validator Validating :::', { schema, payload })
+  log.info('Validator Validating :::', { payload })
 
   const info = schema.safeParse(payload)
   if (info.success) return info.data
