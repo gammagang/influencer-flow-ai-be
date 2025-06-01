f136cf7d09917730c43dc4ef87b7faf212575db0
gammaSpeck <madhukm3141@gmail.com>
2025-06-02 04:04:53 +0530

    chore(public): add note about JWT middleware absence for public routes

--- src/routes/public/route.ts
+++ src/routes/public/route.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express'
 import { SuccessResponse } from '@/libs/success-response'
 
 const router = Router()
+// NOTE: All public routes will have no JWT middleware
 
 router.post('/', async (req: Request, res: Response) => {
   SuccessResponse.send({ res, data: null, status: 201 })
